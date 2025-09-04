d73f6f79463aed000ad27a36c70c045e55583ef6
André <Nelcrack010@gmail.com>
2025-09-03 21:34:53 -0500

    Nelson 1

--- src/capaUsuario/Main.java
+++ src/capaUsuario/Main.java
@@ -17,6 +17,7 @@ public class Main {
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
         System.out.println("Hola mundo");
+        System.out.println("Hola de Nelson");
     }
     
 }
